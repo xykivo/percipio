35b6b7b5aa4a0aa195890b7987a39a9a0c1eb730
Dror Smolarsky <dror.smolarsky@gmail.com>
2020-06-03 22:00:13 -0400

    Add a few function to construct JSON using nlohmann json
    * Add std=c++17 compile flag to nl_json/hellojson target
    * Add creation of simple JSON data structures - map and array

--- nl_json/src/main.cc
+++ nl_json/src/main.cc
@@ -13,11 +13,45 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+#include <array>
 #include <iostream>
+#include <utility>
 
 #include "nlohmann/json.hpp"
+
+void CreateSimpleJson() {
+  nlohmann::json json;
+  json["integer"] = 17;
+  json["float"] = 17.0F;
+  json["string"] = "seventeen";
+  std::cout << "simple json\n" << json << "\n\n";
+}
+
+void CreateJsonArray() {
+  nlohmann::json json;
+  for (int i = 0; 3 > i; ++i) {
+    json["int_array"][i] = i;
+  }
+  std::cout << "json array\n" << json << "\n\n";
+}
+
+void CreateJsonMap() {
+  nlohmann::json json;
+  static constexpr std::array<std::pair<const char*, int>, 3> kMap{
+      std::make_pair("integer_0", 0),
+      std::make_pair("integer_1", 1),
+      std::make_pair("integer_2", 2),
+  };
+  for (const auto& [key, value] : kMap) {
+    json["int_map"][key] = value;
+  }
+  std::cout << "json map\n" << json << "\n\n";
+}
 
 int main() {
   std::cout << "Hello Niels Lohmann JSON\n";
   std::cout << "Sample code on how to use Niels Lohmann JSON library.\n\n";
+  CreateSimpleJson();
+  CreateJsonArray();
+  CreateJsonMap();
 }
