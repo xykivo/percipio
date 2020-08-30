ba6e02922df4e4d06567ecebb2e5d79def78b6c1
Dror Smolarsky <xykivo@gmail.com>
2020-08-30 08:48:21 -0400

    Add simple sample of using C++ std::copy_if algorithm

---
+++ cc/algorithm/copy_if/src/main.cc
@@ -0,0 +1,38 @@
+// Copyright (C) 2020 Dror Smolarsky
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+#include "xykivo/percipio/util/out_stream.h"
+
+/// C++ count main entry point
+int main() {
+  std::cout << "STL std::copy_if samples\n";
+  static constexpr std::array<int, 8> kSrcArray{0, -1, 2, -3, 4, -5, 6, -7};
+  std::cout << "Copied negative numbers from [";
+  xykivo::percipio::util::OutputRange(std::cout, kSrcArray.begin(),
+                                      kSrcArray.end(), " ");
+  std::array<int, 8> dst_array{};
+  auto is_negative = [](int i) { return (0 > i); };
+  std::copy_if(kSrcArray.begin(), kSrcArray.end(), dst_array.begin(),
+               is_negative);
+  std::cout << "] to [";
+  xykivo::percipio::util::OutputRange(std::cout, dst_array.begin(),
+                                      dst_array.end(), " ");
+  std::cout << "]\n";
+  return 0;
+}
