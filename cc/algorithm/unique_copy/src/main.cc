6b63abe5b14d94e0174e2feae7904a0b626a9e68
Dror Smolarsky <dror.smolarsky@gmail.com>
2021-01-25 10:48:08 -0500

    Add simple sample of using C++ std::unique_copy algorithm

---
+++ cc/algorithm/unique_copy/src/main.cc
@@ -0,0 +1,52 @@
+// Copyright (C) 2020 Dror Smolarsky
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+int main() {
+  std::cout << "STL std::unique_copy example\n";
+  static constexpr size_t kArraySize{16};
+  std::array<int, kArraySize> src_array{
+      0, 1, 2, 2, 3, 3, 3, 4,   // NOLINT(readability-magic-numbers)
+      4, 4, 4, 5, 5, 5, 5, 5};  // NOLINT(readability-magic-numbers)
+  std::cout << "Equality unique - array=[";
+  auto print_int = [](int i) { std::cout << i << ' '; };
+  std::for_each(src_array.begin(), src_array.end(), print_int);
+  std::array<int, kArraySize> dst_array{};
+  auto* unique_array_end =
+      std::unique_copy(src_array.begin(), src_array.end(), dst_array.begin());
+  std::cout << "] -> array=[";
+  std::for_each(dst_array.begin(), unique_array_end, print_int);
+  std::cout << "]\n";
+
+  std::cout << "Predicate (equal LSB) unique - array=[";
+  src_array = {0, 1, 3, 5, 7, 2, 4, 6,   // NOLINT(readability-magic-numbers)
+               8, 1, 2, 3, 4, 5, 6, 7};  // NOLINT(readability-magic-numbers)
+  // dst_array = {};
+  std::for_each(src_array.begin(), src_array.end(), print_int);
+  auto equal_lsb = [](int i0, int i1) {
+    static constexpr int kLsbMask{0x1};
+    return ((i0 & kLsbMask) == (i1 & kLsbMask));
+  };
+  unique_array_end = std::unique_copy(src_array.begin(), src_array.end(),
+                                      dst_array.begin(), equal_lsb);
+  std::cout << "] -> array=[";
+  std::for_each(dst_array.begin(), unique_array_end, print_int);
+  std::cout << "]\n";
+
+  return 0;
+}
