bfc8055afe696bace54a6bf317296639206d64d6
Dror Smolarsky <dror.smolarsky@gmail.com>
2021-07-21 09:58:55 -0400

    Add simple sample of using C++ STL is_sorted_until algorithm

---
+++ cc/algorithm/is_sorted_until/src/main.cc
@@ -0,0 +1,54 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2020-2021, Dror Smolarsky
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+
+int main() {
+  std::cout << "C++ STL std::is_sorted_until example\n";
+  static constexpr size_t kArraySize{8};
+  std::array<int, kArraySize> array{
+      0, 1, 2, 3, 4, -1, -2, -3};  // NOLINT(readability-magic-numbers)
+  auto print_range = [](auto begin, auto end) {
+    std::cout << "range=[";
+    auto print_int = [](int i) { std::cout << i << ' '; };
+    std::for_each(begin, end, print_int);
+    std::cout << "] ";
+    std::cout << (std::is_sorted(begin, end) ? "" : "not");
+    std::cout << " sorted\n";
+  };
+  print_range(array.begin(), array.end());
+  auto end_of_sorted_range = std::is_sorted_until(array.begin(), array.end());
+  print_range(array.begin(), end_of_sorted_range);
+  print_range(end_of_sorted_range, array.end());
+  return 0;
+}
