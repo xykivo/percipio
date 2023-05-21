e69c02fbd74e0713bba555a49dffa7b572048d49
Dror Smolarsky <xykivo@gmail.com>
2023-05-21 17:46:16 -0400

    Go: add tour methods example

---
+++ go/tour/t04methods00/go.mod
@@ -0,0 +1,3 @@
+module xykivo/percipio/tour/t04methods00
+
+go 1.20
---
+++ go/tour/t04methods00/main.go
@@ -0,0 +1,74 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2023, Dror Smolarsky
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+// Tour: https://go.dev/tour/methods/1
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vector3D struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+func (v Vector3D) Length() float64 {
+	return math.Sqrt((v.X * v.X) + (v.Y * v.Y) + (v.Z * v.Z))
+}
+
+func (v *Vector3D) Negate() {
+	v.X = -v.X
+	v.Y = -v.Y
+	v.Z = -v.Z
+}
+
+type MyInt int
+
+func (i MyInt) Neg() int {
+	return -(int(i))
+}
+
+func main() {
+	fmt.Println("methods")
+	v := Vector3D{1, 1, 1}
+	fmt.Println("length of", v, "is", v.Length())
+	pv := &v
+	fmt.Println("length of", pv, "is", pv.Length())
+	fmt.Print("negate ", v, " -> ")
+	v.Negate()
+	fmt.Println(v)
+	i := MyInt(1)
+	fmt.Println("The negative of", i, "is", i.Neg())
+}
